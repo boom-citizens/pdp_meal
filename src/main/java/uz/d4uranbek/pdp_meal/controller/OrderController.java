4ceb6ca33770d86a42e9471a59a5488befc75c4c
D4uranbek <d4uranbek@gmail.com>
2022-03-02 19:12:05 +0500

    meal and order controller

--- src/main/java/uz/d4uranbek/pdp_meal/controller/OrderController.java
+++ src/main/java/uz/d4uranbek/pdp_meal/controller/OrderController.java
@@ -3,19 +3,18 @@ package uz.d4uranbek.pdp_meal.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uz.d4uranbek.pdp_meal.service.meal.MealServiceImpl;
+import uz.d4uranbek.pdp_meal.service.order.OrderServiceImpl;
 
 /**
- * @author D4uranbek ср. 18:58. 02.03.2022
+ * @author D4uranbek ср. 19:10. 02.03.2022
  */
+
 @RestController
-@RequestMapping("/api/meal")
-public class MealController extends AbstractController<MealServiceImpl> {
+@RequestMapping("/api/order")
+public class OrderController extends AbstractController<OrderServiceImpl> {
 
     @Autowired
-    protected MealController(MealServiceImpl service) {
+    protected OrderController(OrderServiceImpl service) {
         super(service);
     }
-
-
 }
