d07b35fdf6400b2aa9488f523c0f8467169f838e
D4uranbek <d4uranbek@gmail.com>
2022-03-02 20:08:37 +0500

    validation meal

--- src/main/java/uz/d4uranbek/pdp_meal/validator/Validator.java
+++ src/main/java/uz/d4uranbek/pdp_meal/validator/Validator.java
@@ -1,4 +1,5 @@
 package uz.d4uranbek.pdp_meal.validator;
 
 public interface Validator {
+    String DATE_REGEX = "^((?:19|20)[0-9][0-9])/(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])$";
 }
--- src/main/java/uz/d4uranbek/pdp_meal/validator/meal/MealValidator.java
+++ src/main/java/uz/d4uranbek/pdp_meal/validator/meal/MealValidator.java
@@ -6,6 +6,8 @@ import uz.d4uranbek.pdp_meal.dto.meal.MealUpdateDto;
 import uz.d4uranbek.pdp_meal.exception.ValidationException;
 import uz.d4uranbek.pdp_meal.validator.AbstractValidator;
 
+import java.util.Objects;
+
 /**
  * @author D4uranbek ср. 18:13. 02.03.2022
  */
@@ -19,11 +21,23 @@ public class MealValidator extends AbstractValidator<MealCreateDto, MealUpdateDt
 
     @Override
     public void validOnCreate(MealCreateDto mealCreateDto) throws ValidationException {
+        if (!mealCreateDto.getDate().matches(DATE_REGEX)) {
+            throw new ValidationException("Invalid date format");
+        }
 
+        if (Objects.isNull(mealCreateDto.getName()) || "".equals(mealCreateDto.getName())) {
+            throw new ValidationException("Name should not be blank");
+        }
     }
 
     @Override
-    public void validOnUpdate(MealUpdateDto cd) throws ValidationException {
+    public void validOnUpdate(MealUpdateDto mealUpdateDto) throws ValidationException {
+        if (!mealUpdateDto.getDate().matches(DATE_REGEX)) {
+            throw new ValidationException("Invalid date format");
+        }
 
+        if (Objects.isNull(mealUpdateDto.getName()) || "".equals(mealUpdateDto.getName())) {
+            throw new ValidationException("Name should not be blank");
+        }
     }
 }
