3c4d639556e76b321094639903fb25be7c52e22f
D4uranbek <d4uranbek@gmail.com>
2022-03-02 12:58:38 +0500

    dtos

---
+++ src/main/java/uz/d4uranbek/pdp_meal/dto/meal/MealCreateDto.java
@@ -0,0 +1,7 @@
+package uz.d4uranbek.pdp_meal.dto.meal;
+
+/**
+ * @author D4uranbek ср. 12:57. 02.03.2022
+ */
+public class MealCreateDto {
+}
---
+++ src/main/java/uz/d4uranbek/pdp_meal/dto/order/OrderCreateDto.java
@@ -0,0 +1,7 @@
+package uz.d4uranbek.pdp_meal.dto.order;
+
+/**
+ * @author D4uranbek ср. 12:57. 02.03.2022
+ */
+public class OrderCreateDto {
+}
