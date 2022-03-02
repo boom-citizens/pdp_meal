9149a77054bac103e491027657b1b5f6c889c75e
D4uranbek <d4uranbek@gmail.com>
2022-03-02 06:54:44 +0500

    initial

---
+++ src/main/java/uz/d4uranbek/pdp_meal/PdpMealApplication.java
@@ -0,0 +1,13 @@
+package uz.d4uranbek.pdp_meal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpMealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdpMealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/d4uranbek/pdp_meal/PdpMealApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.d4uranbek.pdp_meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdpMealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
